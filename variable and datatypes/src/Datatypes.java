0b4af05eaf6684f02c1098958cf12d29815b6f04
aknair19 <abhisheknair1441@gmail.com>
2024-06-22 06:39:09 +0530

    add initial lectures files

---
+++ variable and datatypes/src/Datatypes.java
@@ -0,0 +1,14 @@
+// package variable and datatypes.src;
+
+public class Datatypes {
+    public static void main(String args[]) {
+        byte number = 8; // 356 number can be stored
+        System.out.println(number);
+        char ch = 'a'; // single alphabel
+        System.out.println(ch);
+        boolean bool = true;
+        float price = 10.5f;
+        int num = 200;
+
+    }
+}
---
+++ variable and datatypes/src/Variables.java
@@ -0,0 +1,9 @@
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 5;
+        String b = "Hello";
+        System.out.println(a + b);
+    }
+}
