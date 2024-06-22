d39d911fd5364bb538c2c164bf3c3d55181c276f
Abhishek Nair <abhisheknair1441@gmail.com>
2024-06-22 13:33:53 +0530

    sum of and b

---
+++ notes/Introduction(1).md
@@ -0,0 +1,16 @@
+Datatypes
+
+- Char int float long double boolean are called primitive datatypes
+- In primitive datatypes a deepcopy is created.
+- In reference datatypes a shallow copy is created
+- IN C++ we have STL framework for important data structures
+- In Java we have collection framework
+- In collections what ever datatypes we are using we should also use reference datatypes
+
+Array
+
+- int arr[] = new int [5];
+- Array is reference type
+- Array maximum size 10 to power 6
+-
+-
--- variable and datatypes/src/Datatypes.java
+++ variable and datatypes/src/Datatypes.java
@@ -2,13 +2,18 @@
 
 public class Datatypes {
     public static void main(String args[]) {
-        byte number = 8; // 356 number can be stored
-        System.out.println(number);
-        char ch = 'a'; // single alphabel
-        System.out.println(ch);
-        boolean bool = true;
-        float price = 10.5f;
-        int num = 200;
+        // byte number = 8; // 356 number can be stored
+        // System.out.println(number);
+        // char ch = 'a'; // single alphabet
+        // System.out.println(ch);
+        // boolean bool = true;
+        // float price = 10.5f;
+        // int num = 200;
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
 
     }
 }
