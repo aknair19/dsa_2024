493568103a109c929523bc7a8e614fa1350f35c3
Abhishek Nair <abhisheknair1441@gmail.com>
2024-06-23 12:20:09 +0530

    type casting and conversion

---
+++ variable and datatypes/src/TypeCastingAndConversion.java
@@ -0,0 +1,9 @@
+public class TypeCastingAndConversion {
+    public static void main(String[] args) {
+        // double number = 8.6578;
+        // int number2 = (int) number;
+        int $ = 24;
+        System.out.println($);
+        // System.out.println(number2);
+    }
+}
