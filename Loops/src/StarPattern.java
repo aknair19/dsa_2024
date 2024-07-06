fbd97a3e7d4a4adf209b224cd48607165d1c4218
aknair19 <abhisheknair1441@gmail.com>
2024-07-06 15:20:08 +0530

    pateerns

--- Loops/src/StarPattern.java
+++ Loops/src/StarPattern.java
@@ -12,7 +12,7 @@ public class StarPattern {
         // inverted star
 
         int n = 5;
-
+        // pattern is star is decreasing as the lines are increasing. so
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < n - i + 1; j++) {
                 System.out.print("*");
