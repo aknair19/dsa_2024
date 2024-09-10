58d5764d1558cb300e66bee05dce3c8c8f2378fd
aknair19 <abhisheknair1441@gmail.com>
2024-09-10 11:17:08 +0530

    find largest and smallest in an array

--- Arrays/src/LargestInArray.java
+++ Arrays/src/LargestInArray.java
@@ -1,5 +1,29 @@
 public class LargestInArray {
+
+    public static int getLargestInArray(int list[]) {
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] > result) {
+                result = list[i];
+            }
+        }
+        return result;
+    }
+
+    public static int getSmallestInArray(int list[]) {
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] < result) {
+                result = list[i];
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+        int list[] = { 2, 2, 4, 6, 7, 1, 4, 5, 6 };
+        System.out.println("Max value is " + getLargestInArray(list));
+        System.out.println("Min value is " + getSmallestInArray(list));
 
     }
 }
